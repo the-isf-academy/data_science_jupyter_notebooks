711b132c94ce049594955e7025ca5c9858fba93a
Emma Brown <69293306+emmaqbrown@users.noreply.github.com>
2023-01-03 10:59:52 +0800

    Create full_listening_history.py

---
+++ full_listening_history.py
@@ -0,0 +1,71 @@
+full_listening_history = [{
+    "ms_played":220238,
+    "master_metadata_track_name":"BANG BANG BANG",
+    "master_metadata_album_artist_name":"BIGBANG",
+    "master_metadata_album_album_name":"MADE",
+    "skipped":False
+ },
+ {
+    "ms_played":183920,
+    "master_metadata_track_name":"Beg",
+    "master_metadata_album_artist_name":"Jack & Jack",
+    "master_metadata_album_album_name":"Beg",
+    "skipped":False,
+ },
+ {
+    "ms_played":5316,
+    "master_metadata_track_name":"Younger Now",
+    "master_metadata_album_artist_name":"Miley Cyrus",
+    "master_metadata_album_album_name":"Younger Now",
+    "skipped":False
+ },
+ {
+    "ms_played":243933,
+    "master_metadata_track_name":"Brand New",
+    "master_metadata_album_artist_name":"Ben Rector",
+    "master_metadata_album_album_name":"Brand New",
+    "skipped":False
+ },
+ {
+    "ms_played":13200,
+    "master_metadata_track_name":"Galway Girl",
+    "master_metadata_album_artist_name":"Ed Sheeran",
+    "master_metadata_album_album_name":"÷",
+    "skipped":False
+ },
+ {
+    "ms_played":158413,
+    "master_metadata_track_name":"Si un jour",
+    "master_metadata_album_artist_name":"La Femme",
+    "master_metadata_album_album_name":"Psycho Tropical Berlin",
+    "skipped":False
+ },
+ {
+    "ms_played":100334,
+    "master_metadata_track_name":"I Think I Love You",
+    "master_metadata_album_artist_name":"The Partridge Family",
+    "master_metadata_album_album_name":"Partridge Family Album",
+    "skipped":False
+ },
+ {
+    "ms_played":2043,
+    "master_metadata_track_name":"Isn't It A Lovely Night?",
+    "master_metadata_album_artist_name":"The Decemberists",
+    "master_metadata_album_album_name":"Hazards Of Love",
+    "skipped":True
+ },
+ {
+    "ms_played":84659,
+    "master_metadata_track_name":"Baba O'Riley",
+    "master_metadata_album_artist_name":"The Who",
+    "master_metadata_album_album_name":"The Who Hits 50",
+    "skipped":True
+ },
+ {
+    "ms_played":12618,
+    "master_metadata_track_name":"All Alone",
+    "master_metadata_album_artist_name":"fun.",
+    "master_metadata_album_album_name":"Some Nights",
+    "skipped":True
+ }
+ ]
